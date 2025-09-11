9ac81ddf76a404c053c9a9a3e73eb4d5780c4192
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-09-11 08:22:25 +0530

    Update rank_clinics.py

--- doctor-summary/rank_clinics.py
+++ doctor-summary/rank_clinics.py
@@ -1,5 +1,13 @@
 # Rank clinics using reviews pulled from QLife.
 import argparse
+from japanese_address import parse as japanese_address_parser
+
+def find_prefecture_ward_city(address):
+    parsed_address = japanese_address_parser(address)
+    prefecture = parsed_address.get("prefecture", "")
+    ward = parsed_address.get("ward", "")
+    city = parsed_address.get("city", "")
+    return prefecture, ward, city
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Rank clinics using reviews")
@@ -7,4 +15,4 @@ if __name__ == "__main__":
     address = parser.add_argument("--address", type=str, default="山梨県中央市", help="Address to search")
     args = parser.parse_args()
 
-    
+    prefecture, ward, city = find_prefecture_ward_city(args.address)
